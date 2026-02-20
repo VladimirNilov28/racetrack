a516edc7d9f6479038e2819b0c773815d1b9515e
vladimirnilov <nilov.vova28@gmail.com>
2026-02-20 22:12:15 +0200

    Some minor fixes

--- backend/src/security/global-key-control.js
+++ backend/src/security/global-key-control.js
@@ -1,62 +1,43 @@
 import { env } from "node:process";
 import logger from "../logger.js";
 
-/**
- * HOW TO DEFINE YOUR ACCESS KEYS:
- *
- * export RECEPTIONIST_KEY=<your key>
- * export SAFETY_KEY=<your key>
- * export OBSERVER_KEY=<your key>
- */
-
 // NOTE: do NOT log actual key values
-const RECEPTIONIST_KEY = env.RECEPTIONIST_KEY || "admin";
-const SAFETY_KEY = env.SAFETY_KEY || "admin";
-const OBSERVER_KEY = env.OBSERVER_KEY || "admin";
+const RECEPTIONIST_KEY = env.RECEPTIONIST_KEY;
+const SAFETY_KEY = env.SAFETY_KEY;
+const OBSERVER_KEY = env.OBSERVER_KEY;
 
 const info = `
-Configure:
+Missing Required Environment Variables!
+
+Please configure your access keys before starting the server:
   export RECEPTIONIST_KEY=<your key>  Define access key for /front-desk
   export SAFETY_KEY=<your key>        Define access key for /race-control
   export OBSERVER_KEY=<your key>      Define access key for /lap-line-tracker
 `;
 
 export const keyCheck = () => {
-  logger.info("server:env:check", {
-    receptionistKeyPresent: !!env.RECEPTIONIST_KEY,
-    safetyKeyPresent: !!env.SAFETY_KEY,
-    observerKeyPresent: !!env.OBSERVER_KEY,
-  });
-
-  if (!RECEPTIONIST_KEY) {
-    logger.error("server:env:missing", {
-      key: "RECEPTIONIST_KEY",
-      hint: info,
+    logger.info("server:env:check", {
+        receptionistKeyPresent: !!RECEPTIONIST_KEY,
+        safetyKeyPresent: !!SAFETY_KEY,
+        observerKeyPresent: !!OBSERVER_KEY,
     });
-    process.exit(1);
-  }
 
-  if (!SAFETY_KEY) {
-    logger.error("server:env:missing", {
-      key: "SAFETY_KEY",
-      hint: info,
-    });
-    process.exit(1);
-  }
+    const missingKeys = [];
+    if (!RECEPTIONIST_KEY) missingKeys.push("RECEPTIONIST_KEY");
+    if (!SAFETY_KEY) missingKeys.push("SAFETY_KEY");
+    if (!OBSERVER_KEY) missingKeys.push("OBSERVER_KEY");
 
-  if (!OBSERVER_KEY) {
-    logger.error("server:env:missing", {
-      key: "OBSERVER_KEY",
-      hint: info,
-    });
-    process.exit(1);
-  }
 
-  logger.info("server:env:ok");
+    if (missingKeys.length > 0) {
+
+        logger.error("server:env:missing", { missingKeys });
+
+        console.error(info);
+
+        process.exit(1);
+    }
+
+    logger.info("server:env:ok");
 };
 
-export {
-  RECEPTIONIST_KEY,
-  SAFETY_KEY,
-  OBSERVER_KEY,
-};
+export { RECEPTIONIST_KEY, SAFETY_KEY, OBSERVER_KEY };
